8b477b1bfc2360f0bc0752c5082f22e09bdacc1f
RangeWING <rangewing@kaist.ac.kr>
2017-12-21 00:39:39 +0900

    Image folder created

--- bmpENC/main.c
+++ bmpENC/main.c
@@ -16,7 +16,7 @@ int main() {
 	uint w, h;
 	BMPImage *img, *newimg;
 
-	const char bmpfile[256] = "black.bmp";
+	const char bmpfile[256] = "img/black.bmp";
 	char data[] = "--data?--testdata Hello,world!\nHELLO HELLO Repeat";
 	uint blen;
 	uint8_t *dbit = strToBit(data, &blen);
@@ -27,7 +27,7 @@ int main() {
 
 	newimg = enc_evenodd_gray(img, data);
 
-	writeBMP(newimg, "test.bmp");
+	writeBMP(newimg, "img/test.bmp");
 
 	closeBMP(img);
 	closeBMP(newimg);
@@ -35,7 +35,7 @@ int main() {
 	system("pause");
 
 
-	img = openBMP("test.bmp");
+	img = openBMP("img/test.bmp");
 
 	char *str = dec_evenodd_gray(img);
 
